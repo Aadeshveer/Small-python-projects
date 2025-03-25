48394d9b0b0ae292c4d1f58c075c7217f431ab60
Aadeshveer <24b0926@iitb.ac.in>
2025-03-25 03:48:52 +0000

    Made dice roller program

---
+++ 18.Dice_roller/roller.py
@@ -0,0 +1,23 @@
+import re
+import random
+
+print(" Dice Roller ".center(60,'*'))
+while True:
+    while True:
+        x = input("Enter command with syntax no_of_dice d no_of_sides_of_dice + modifier\n> ")
+        if x.lower()=='q':
+            exit(0)
+        if re.search("^[0-9]+ *d *[0-9]+ *([+-] *[0-9]+){0,1}$",x)==None:
+            print("Invalid input!!")
+        else:
+            break
+    num,side,mod = re.match("^([0-9]+) *d *([0-9]+) *([+-] *[0-9]+){0,1}$",x).groups()
+    rolls = []
+    result = 0
+    for _ in range(int(num)):
+        roll = random.randint(1,int(side))
+        result+=roll
+        rolls.append(roll)
+    result+=int(mod)
+    rolls.append(mod)
+    print(result,tuple(rolls))
