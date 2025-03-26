e42f6b8d7b092f118459dfa9b3fee68b47476893
Aadeshveer <24b0926@iitb.ac.in>
2025-03-26 03:34:08 +0000

    Created Ducklings class in screensaver, implemented duck generating code

--- Collective_resources/screensaver.py
+++ Collective_resources/screensaver.py
@@ -1,5 +1,6 @@
 import random
 import time
+import sys
 
 
 class BitStream():
@@ -136,3 +137,109 @@ class DNAVisualization():
             print(self.get_str())
             self.iterate()
             time.sleep(self.delay)
+
+class Ducklings():
+    '''
+    Generates a running stream of ascii art ducklings
+    '''
+    DIRECTIONS = ['left','right']
+    BODIES = [[' ({} )','  ^^ '],['({}  )',' ^ ^ ']]
+    EYES = ['"',"''","^^",'``']
+    MOUTHS = ['<','=']
+    WINGS = ['<','V','^']
+    def __init__(self, number = 10, width = 60, delay = 0.01):
+        self.num = number
+        self.width = 60
+        self.delay = delay
+
+    def duck_gen(self, direction='right', body_type=0, eye_type=0, mouth_type=0, wing_position=0):
+        '''
+        Generates  a  duckling ascii art
+        
+        Args:
+            direction: can be 'left' or 'right'
+            body_size: can be a non -ve index for BODIES list
+            eye_type: can be a non -ve index for EYES list
+            mouth_type: can be a non -ve index for MOUTHS list
+            wing_position: can be a non -ve index for WINGS list
+
+        returns:
+            A list of sttrings which form the duckling when printed line by line
+
+        raises:
+            ValueError: If given arguements are not correct
+        '''
+        if direction not in self.DIRECTIONS:
+            raise ValueError("Given direction arguement is invalid")
+        if body_type not in range(len(self.BODIES)):
+            raise ValueError("Given body_type is not invalid")
+        if eye_type not in range(len(self.EYES)):
+            raise ValueError("Given eye_type is invalid")
+        if mouth_type not in range(len(self.MOUTHS)):
+            raise ValueError("Given mouth_type is invalid")
+        if wing_position not in range(len(self.WINGS)):
+            raise ValueError("Given wing_position is invalid")
+        
+        return [
+            self.__head_str(direction,eye_type,mouth_type),
+            self.__body_str(direction,body_type,wing_position),
+            self.__feet_str(direction,body_type)
+            ]
+    
+    def __head_str(self,direction,eye_type,mouth_type):
+        '''
+        Generates the head string of duck
+        '''
+        eyes = self.EYES[eye_type]
+        s = (' ' if len(eyes)==2 else '  ')+'('+eyes+self.MOUTHS[mouth_type]
+        if direction=='left':
+            s = ''.join(self.__dir_inv(list(s)))
+        return s
+    
+    def __body_str(self,direction,body_type,wing_position):
+        '''
+        Generates the body string of duck
+        '''
+        s = self.BODIES[body_type][0].format(self.WINGS[wing_position])
+        if direction=='left':
+            s = ''.join(self.__dir_inv(list(s)))
+        return s
+
+    def __feet_str(self,direction,body_type):
+        '''
+        Generats the feet string
+        '''
+        s = self.BODIES[body_type][1]
+        if direction=='left':
+            s = ''.join(self.__dir_inv(list(s)))
+        return s
+
+    def __dir_inv(self,lst):
+        '''
+        Reverses the string to change direction
+        
+        Args:
+            lst: list of characters in string to reverse
+        
+        returns:
+            list of characters in invereted string
+        '''
+        lst.reverse()
+        if '<' in lst:
+            while lst.count('<') != 0:
+                lst[lst.index('<')] = '>'
+        else:
+            while lst.count('>') != 0:
+                lst[lst.index('>')] = '<'
+        opening_brackets = []
+        
+        for _ in range(lst.count('(')):
+            opening_brackets.append(lst.index('('))
+        
+        while lst.count(')') !=0:
+            lst[lst.index(')')] = '('
+
+        for i in opening_brackets:
+            lst[i] = ')'
+                
+        return lst
